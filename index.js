d60fd98fc06b3562736aabd5274bb01ae6b5a14c
Ricardo <rferromoreno@gmail.com>
2019-09-28 19:19:25 -0300

    Add logic to write at output file. Add logic for checking input and output paths

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import fs from 'fs';
+import yargs from 'yargs';
 import { replacer } from './utils';
 import { DATE_REGEX } from './constants';
 
-const filename = './example.txt';
+const argv = yargs
+  .usage('Usage: $0 <input> <output> [options]')
+  .help('h')
+  .alias('h', 'help').argv;
 
-const content = fs.readFileSync(filename, 'utf8');
-const newContent = content.replace(DATE_REGEX, replacer);
+const input = argv._[0];
+const output = argv._[1];
 
-console.log(newContent);
+try {
+  if (!input) {
+    console.error('Missing input file');
+    throw Error('NO_INPUT_FILE');
+  }
+  if (!output) {
+    console.error('Missing output file');
+    throw Error('NO_OUTPUT_FILE');
+  }
+
+  const content = fs.readFileSync(input, 'utf8');
+  const newContent = content.replace(DATE_REGEX, replacer);
+  fs.writeFileSync(output, newContent);
+
+  console.log('Ready!');
+} catch (err) {
+  console.error("Couldn't complete the request");
+}
